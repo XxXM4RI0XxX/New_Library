61dd2d26011f693e16db17f93a9f0c33a0740847
XxXM4RI0XxX <montejanodiaza@gmail.com>
2024-05-02 23:47:27 -0600

    Just mods

--- src/Library_II/Test_class.java
+++ src/Library_II/Test_class.java
@@ -6,5 +6,11 @@ public class Test_class {
     public static void main(String[]args){
 
         byte a = 'a';
+        // ArrayList -> Mas eficiente al buscar datos, vale vrg al borrar y agregar datos en medio del array...
+        //... Esto debido a que como usa arrays para funcionar, si es un array muy grande, ocupara muchos recursos.
+        // LinkedList -> Vale vrg pa buscar datos, pero es super efectivo al momento de borrar y agregar datos en posiciones especificas...
+        //... Esto debido a que utiliza pointers para encontrar los datos y moverlos, haciendolo super efectivo al manipularlos, pero no al buscarlos; por que
+        // al buscarlos va pointer por pointer hasta encopntrar el dato requerido.
+
     }
 }
